1fc2c532f85289baac147fad187ae52a5f33a48d
reffas chouaib <reffaschouaib@gmail.com>
2026-01-21 20:12:25 +0100

    [UPDATED] : Academic menu screen with improved styling integration

--- Application/GUI/screens/academic_menu_screen.py
+++ Application/GUI/screens/academic_menu_screen.py
@@ -11,6 +11,9 @@ from PyQt6.QtWidgets import (
 
 import logging
 
+from GUI.styling import styling_loader as sl
+
+
 logger = logging.getLogger(__name__)
 
 
@@ -29,6 +32,12 @@ class AcademicMenu(QWidget):
     def __init__(self):
         super().__init__()
         uic.loadUi(PATH_TO_UI, self)
+ 
         self.back_btn.clicked.connect(self.go_back.emit)
+ 
         self.IC_Assignments.clicked.connect(self.navigate_to_assign_instructor.emit)
+ 
         self.reservation_managment.clicked.connect(self.navigate_to_manage_reservation.emit)
+
+        #* Set the button styles
+        sl.apply_button_styles(self,sl.ACADEMIC_MENU_STYLES)
